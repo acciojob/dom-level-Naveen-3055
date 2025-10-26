7d56c2298f3512c63cd96ecb0cd54fa1747c4b33
Naveen Kumar <knaveenk869@gmail.com>
2025-10-26 11:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function getLevel(element) {
+	if(!element) return -1;
+	let depth =0;
+	const current = element
+	while(current && current !== document.documentElement){
+		depth++;
+		current = current.parentElement;
+	}
+	return depth;
+}
+const element = document.getElementById('level')
+alert(`The level of the element is : ${getLevel(element)}`)
